4d32e1a81a15582bcf60c6eee4a1961aa8da07d2
movingroovin <finnbloo@gmail.com>
2021-08-23 00:48:27 +0800

    landing page update

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,11 +8,18 @@ import './../assests/style/Index.scss'
 const IndexPage = () => {
   return (
     <div className="container">
+      <div className="inner-header">
+        <div className="inner-header-icon">W</div>
+        <div className="inner-header-content">
+          <a className="inner-header-content-item" href="https://github.com/movingroovin" target="_blank" rel="noreferrer" aria-label='GitHub'>GITHUB</a>
+          <a className="inner-header-content-item" href="https://www.linkedin.com/in/cheng-wei-wei-chen-179a2a183/" target="_blank" rel="noreferrer" aria-label='LinkedIn'>LINKEDIN</a>
+        </div>
+      </div>
       <div className="inner">
         <div className="main-name">
           <div className="main-name-item">Cheng Wei</div>
-          {/* <div className="main-name-item">weei</div>
-          <div className="main-name-item">weii</div> */}
+          {/* <div className="main-name-item">CHEN</div> */}
+          {/* <div className="main-name-item">weii</div> */}
         </div>
       </div>
       <div className="tags">
